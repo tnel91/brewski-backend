9843edc561a4892a0e2d9c30d142fd013d163106
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-11-15 12:41:08 -0700

    Added user routes

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -10,5 +10,13 @@ router.put('/:brewery_id', controller.updateBrewery)
 
 router.delete('/:brewery_id', controller.deleteBrewery)
 
+router.get('/user', controller.getUsers)
+
+router.post('/users/new', controller.createUser)
+
+router.put('/:user_id', controller.updateUser)
+
+router.delete('/:user_id', controller.deleteUser)
+
 //routes here
 module.exports = router
