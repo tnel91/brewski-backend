2a8ed29123ebfa89ab7af7a3be439ce37ba40cc1
Travis Nelson <tjnel6691@gmail.com>
2022-11-15 11:19:33 -0500

    Added migration to remove review_brewery table

---
+++ migrations/20221115161535-remove-review_brewery-table.js
@@ -0,0 +1,51 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.dropTable('review_breweries')
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.createTable('review_breweries', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'reviews',
+          key: 'id'
+        }
+      },
+      breweryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'breweries',
+          key: 'id'
+        }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    })
+  }
+}
