cefa76dbec502cd2e057cba1229a0a73a361c8a4
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-11-15 13:46:08 -0700

    Added review routes in contollers and updated appRouter.js

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const controller = require('../controllers/Controller')
 
 //routes here
+
+//Breweries
+
 router.get('/brewery', controller.getBrewery)
 
 router.post('/create_router', controller.createBrewery)
@@ -10,6 +13,8 @@ router.put('/:brewery_id', controller.updateBrewery)
 
 router.delete('/:brewery_id', controller.deleteBrewery)
 
+//User
+
 router.get('/user', controller.getUsers)
 
 router.post('/users/new', controller.createUser)
@@ -17,6 +22,16 @@ router.post('/users/new', controller.createUser)
 router.put('/:user_id', controller.updateUser)
 
 router.delete('/:user_id', controller.deleteUser)
+
+//Reviews
+
+router.get('/reviews', controller.getReview)
+
+router.post('/reviews/new', controller.createReview)
+
+router.put('/:review_id', controller.updateReview)
+
+router.delete('/:review_id', controller.deleteReview)
 
 //routes here
 module.exports = router
