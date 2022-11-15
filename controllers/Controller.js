27794e2125521278b8010a84eda6c56d9d55aca1
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-11-15 10:30:13 -0700

    getBrewery function addded

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,3 +1,35 @@
 const { User, Brewery } = require('../models')
 
-module.exports = {}
+const getBrewery = async (req, res) => {
+  try {
+    const brewerys = await Brewery.findAll()
+    res.send(brewerys)
+  } catch (error) {
+    throw error
+  }
+}
+const getOneBrewery = async (req, res) => {
+  let breweryIds = parseInt(req.params.breweryId)
+}
+const createBrewery = async (req, res) => {
+  try {
+    const brewery = await Brewery.create({ ...req.body })
+    res.send(brewery)
+  } catch (error) {
+    throw error
+  }
+}
+const updateBrewery = async (req, res) => {
+  try {
+    const brewery = await Brewery.update(
+      { ...req.body },
+      { where: { id: req.params.breweryId }, returning: true }
+    )
+    res.send(brewery)
+  } catch (error) {
+    throw error
+  }
+}
+module.exports = {
+  getBrewery
+}
--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,7 +1,24 @@
 const Router = require('express').Router()
 const controller = require('../controllers/Controller')
-//routes here
+const express = require('express')
+const cors = require('cors')
+const router = express.Router()
 
+const { Review } = require('./models')
+const { Brewery } = require('./models')
+const app = express()
+
+//routes here
+app.get('/breweries', async (req, res) => {
+  const allBrewery = await Brewery.find({})
+  res.json(allBrewery)
+})
+router.get('/', function (req, res) {
+  res.send('Wiki home page')
+})
+// app.post('/breweries', async (req, res) => {
+//   const
+// })
 //routes here
 
 module.exports = Router
